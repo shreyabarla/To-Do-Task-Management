55791c3141d0b09aa5fbe6f2c1e31297bd0df189
Inchara P Jain <incharajain.puttur@gmail.com>
2026-07-02 19:58:46 +0530

    Setup backend architecture with MongoDB and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
+
+const connectDB = require("./config/db");
+const {
+    notFound,
+    errorHandler,
+} = require("./middleware/errorMiddleware");
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Task Management Backend is Running!");
+    res.send("Server Running...");
 });
 
-const PORT = 5000;
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
